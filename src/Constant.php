c60eef01fbfa4aed63bf5ea52dcfdd18490d14f4
Bayu29 <bsegara2903@gmail.com>
2022-02-02 00:19:13 +0700

    first commit

---
+++ src/Constant.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BayuDev\Digiflazz;
+
+class Constant 
+{
+    /**
+     * Username Digiflazz
+     */
+     public const USERNAME = '';
+
+    /**
+     * Development Key Digiflazz
+     */
+    public const DEVELOPMENT_KEY = '';
+     
+    /**
+     * Production Key Digiflazz
+     */
+    public const PRODUCTION_KEY = '';
+
+    /**
+    * Development Mode
+    */
+    public const DEVELOPMENT = 'development';
+
+    /**
+     * Production Mode
+     */
+    public const PRODUCTION = 'production';
+
+    /**
+     * URL ENDPOINT
+     */
+    public const ENDPOINT = 'https://api.digiflazz.com/v1';
+
+   
+}
+
+?>
